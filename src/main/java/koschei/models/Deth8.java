65de4f1dffa24412005494dace572535a6039725
ErikMarx <121640897+MarxErik@users.noreply.github.com>
2023-09-19 01:07:46 +0600

    Нашел иглу :)

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "\nПоэтому иглу нужно спрятать и начать новую жизнь, до добра это не доведет";
+    }
 }
